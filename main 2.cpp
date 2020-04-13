8ec5cbc3928749d2f6f701de27cf5244df7dd749
RevekkaKyriakoglou <55149023+RevekkaKyriakoglou@users.noreply.github.com>
2020-04-13 21:35:03 +0200

    Update main 2.cpp

--- main 2.cpp
+++ main 2.cpp
@@ -6,40 +6,40 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int n;
-    float sum, x;
-    int i, j;
+	int n;
+	float sum, x;
+	int i, j;
 
-    float *a1, *a2, **a3, *b1, *b2, **b3;
+	float *a1, *a2, **a3, *b1, *b2, **b3;
 
-	cout<<"Dwse ton arithmo n:"<<endl;
-    cin>>n;
+	cout<<"Enter the number n:"<<endl;
+	cin>>n;
 
-    a1 = new float[n];
-    a2 = new float[n];
-    
-    a3 = new float *[n] ;
-    for( i = 0 ; i < n ; i++ ){
-         a3[i] = new float[n];
-    }
+	a1 = new float[n];
+	a2 = new float[n];
+    	a3 = new float *[n] ;
+	for( i = 0 ; i < n ; i++ )
+    	{
+         	a3[i] = new float[n];
+    	}
     
    	for(i=0; i<=n-1; i++)
    	{	
-      cout<<"Dwse to x_"<<i<<":"<<endl;
-      cin>>a1[i];
-    }
+     		cout<<"Enter x_"<<i<<":"<<endl;
+      		cin>>a1[i];
+   	}
 	    
-    for(i=0;i<=n-1;i++)
-    {
-        cout<<"Dwse tin timi sto x_"<<i<<":"<<endl;
-	    cin>>a2[i];
-	}
-      
+    	for(i=0;i<=n-1;i++)
+    	{
+        	cout<<"Enter the value of x_"<<i<<":"<<endl;
+		cin>>a2[i];
+    	}
+    	
 	for(j=0; j<n; j++)
-	{	
-        if(j==0)
+    	{	
+        	if(j==0)
 		{	
-            for(i=0; i<n; i++)
+            		for(i=0; i<n; i++)
 			{
 				if (i==(n-1))
 				{
@@ -47,152 +47,143 @@ int main(int argc, char *argv[])
 				}
 				else
 				{
-					a3[i][j]=(a2[i]-a2[i+1])/(a1[i]-a1[i+1]);
-                }
+					a3[i][j]=(a2[i]-a2[i+1])/(a1[i]-a1[i+1])
+				}
 			}
 		}
 		else
 		{
 			for(i=0; i<n; i++)
 			{			
-                if(i<n-j-1)
+                		if(i<n-j-1)
 				{
 					a3[i][j]=(a3[i][j-1]-a3[i+1][j-1])/(a1[i]-a1[i+1+j]);
 				}
 				else
 				{
 					a3[i][j]=0;
-                }
+                		}
 			}
 		}
-	}
-	cout<<"O pinakas twn diairemenwn diaforwn einai o "<<endl;
-    for(i=0; i<n; i++){
-             cout<<setw(10)<<a1[i];
-             cout<<setw(10)<<a2[i];
+   	}
+    	cout<<"The matrix of 'diairemenwn diaforwn' is: "<<endl;
+    	for(i=0; i<n; i++)
+	{
+             	cout<<setw(10)<<a1[i];
+             	cout<<setw(10)<<a2[i];
         for (j=0; j<n; j++)
         {
-             if(j<n-i-1)
-				{
-					cout<<setw(10)<<a3[i][j];
-				}
-				else
-				{
-					cout<<setw(10)<<" ";
+             	if(j<n-i-1)
+		{
+			cout<<setw(10)<<a3[i][j];
+		}
+		else
+		{
+			cout<<setw(10)<<" ";
                 }
              
         }
         cout<<endl<<endl;
-    }
+}
         
 	cout<<"Dwste to x:"<<endl;
 	cin>>x;
 
 	while(x>a1[n] || x<a1[0])
 	{
-		cout<<"To x pou dwsate den einai sta epitrepta oria."<<endl<<"Parakalw dwste arithmo mesa sta oria";
+		cout<<"The given x is not within the allowable limits."<<endl<<"Please give a number within the limits";
 		cin>>x;
 	}
 	
-    float gin=1;
-	sum=a2[0];
-	for (i=0; i<n-1; i++)
-	{
-		gin=gin*(x-a1[i]);
-		sum=sum+a3[0][i]*gin;
-	}
-	cout<<"H lysi einai: "<<sum<<endl;
-	
-	
-	
+float gin=1;
+sum=a2[0];
+for (i=0; i<n-1; i++)
+{
+	gin=gin*(x-a1[i]);
+	sum=sum+a3[0][i]*gin;
+}
+cout<<"The solution is: "<<sum<<endl;	
 	
 
-	b1 = new float[n+1];
-    b2 = new float[n+1];
+b1 = new float[n+1];
+b2 = new float[n+1];
     
-    b3 = new float *[n+1] ;
-    for( i = 0 ; i < n+1 ; i++ )
-    {
-         b3[i] = new float[n+1];
-    }
+b3 = new float *[n+1] ;
+for( i = 0 ; i < n+1 ; i++ )
+{
+	b3[i] = new float[n+1];
+}
     
-   	for(i=0; i<=n; i++)
-   	{
-              if(i<n)
-              {         
-                     b1[i]=a1[i];
-              }
-              else if(i=n)
-              {
-                     b1[i]=x;
-              }
-    }
-	  for(i=0; i<=n; i++)
-   	{
-              if(i<n)
-              {         
-                     b2[i]=a2[i];
-              }
-              else if(i=n)
-              {
-                     b2[i]=sum;
-              }
+for(i=0; i<=n; i++)
+{
+	if(i<n)
+        {        
+		b1[i]=a1[i];
+        }
+	else if(i=n)
+        {
+           	b1[i]=x;
+        }
+}
+for(i=0; i<=n; i++)
+{
+       	if(i<n)
+        {         
+             	b2[i]=a2[i];
+         }
+         else if(i=n)
+         {
+                 b2[i]=sum;
+         }
         
-    }  
+}  
    
       
-	for(j=0; j<=n-1; j++)
-	{	
+for(j=0; j<=n-1; j++)
+{	
         if(j==0)
-		{	
-            for(i=0; i<=n; i++)
-			{
-				if (i==n)
-				{
-					b3[i][j]=0;
-				}
-				else
-				{
-					b3[i][j]=(b2[i]-b2[i+1])/(b1[i]-b1[i+1]);
-                }
-              
-			}
-         }
-         
-         
-		else
+	{	
+    		for(i=0; i<=n; i++)
 		{
-			for(i=0; i<=n; i++)
-			{			
-                if(i<n-j)
-				{
-				b3[i][j]=(b3[i][j-1]-b3[i+1][j-1])/(b1[i]-b1[i+1+j]);
-       	}
-				else if(i>=n-j)
-				{
-					b3[i][j]=0;
-               }
-	          
-          }
+			if (i==n)
+			{
+				b3[i][j]=0;
+			}
+			else
+			{
+				b3[i][j]=(b2[i]-b2[i+1])/(b1[i]-b1[i+1]);
+                	}
 		}
-	}
-
-
-	
-	float sf=b3[1][n-2];
-
-	for(i=0; i<n; i++)
+         }
+	else
 	{
-          sf=sf*(b1[n]-b1[i]);
-    }
+		for(i=0; i<=n; i++)
+		{			
+               		if(i<n-j)
+			{
+				b3[i][j]=(b3[i][j-1]-b3[i+1][j-1])/(b1[i]-b1[i+1+j]);
+       			}
+			else if(i>=n-j)
+			{
+				b3[i][j]=0;
+			}
+          	}
+	}
+}
+
 
 	
-	
-	
-	cout<<"Kai to sfalma einai:";
+float sf=b3[1][n-2];
+
+for(i=0; i<n; i++)
+{
+ 	sf=sf*(b1[n]-b1[i]);
+}
+
+cout<<"And the error is:";
 cout<<sf;
 	
 	
-	system("pause");
-    return EXIT_SUCCESS;
+system("pause");
+return EXIT_SUCCESS;
 }
